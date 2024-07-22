3d6a98b9a5dd65f02a5419f42384ebc562c431cf
Inbalc2 <167550250+Inbalc2@users.noreply.github.com>
2024-07-22 13:33:53 +0300

    Update crop_images_gt.py

--- crop_images_gt.py
+++ crop_images_gt.py
@@ -3,6 +3,7 @@ import cv2
 from collections import defaultdict
 from PIL import Image
 import numpy as np
+import argparse
 from cv_algorithms import guo_hall
 
 def create_directory(path):
@@ -35,7 +36,7 @@ def crop_and_save_images(full_image_path, zones, output_path):
         cv2.imwrite(output_filename, crop)
 
 def unify_crops(input_dir, output_dir, crop_size=256, unified_size=128):
-    create_directory(output_dir)  # Ensure the output directory is created
+    create_directory(output_dir)
     files = sorted(os.listdir(input_dir))
     for i in range(0, len(files), 4):
         unified_image = Image.new('RGB', (crop_size, crop_size), (0, 0, 0))
@@ -48,16 +49,15 @@ def unify_crops(input_dir, output_dir, crop_size=256, unified_size=128):
         new_filename = '-'.join(files[i].split('-')[:3]) + '.png'
         unified_image.save(os.path.join(output_dir, new_filename))
 
-def process_images(clemex_path, zones_path, output_path, full_image_ext='png'):
+def process_images(mlography_plus_path, zones_path, output_path, full_image_ext='png'):
     create_directory(output_path)
     mapper = map_images_by_index(zones_path)
     for k in mapper:
-        full_image_path = f'{clemex_path}/{k}.{full_image_ext}'
+        full_image_path = f'{mlography_plus_path}/{k}.{full_image_ext}'
         if not os.path.exists(full_image_path):
             print(f"File {full_image_path} not found. Skipping...")
             continue
         crop_and_save_images(full_image_path, mapper[k], output_path)
-
 
 def apply_guo_hall_thinning(image):
     image = np.array(image)
@@ -72,7 +72,6 @@ def convert_to_color(thinned_image, original_image):
     combined_image = np.where(thinned_image[..., None] == 0, original_image, color_thinned_image)
     return combined_image
 
-# Process each image in the input directory and save the thinned images
 def process_img(input_dir, output_dir):
     for image_name in os.listdir(input_dir):
         if image_name.endswith(('.png', '.jpg', '.tif')):
@@ -89,40 +88,43 @@ def process_img(input_dir, output_dir):
             final_image.save(output_path)
             print(f"Saved processed image: {output_path}")
 
-
 def main():
     parser = argparse.ArgumentParser(description="Process and unify images for metallography.")
-    parser.add_argument('--mlography++_path', type=str, required=True, help='Path to the MLOgraphy++ predictions directory.')
+    parser.add_argument('--mlography_plus_path', type=str, required=True, help='Path to the MLOgraphy++ predictions directory.')
     parser.add_argument('--zones_path', type=str, required=True, help='Path to crops with the zones in their filename.')
     parser.add_argument('--output_path', type=str, required=True, help='Path to the output directory.')
     parser.add_argument('--mlography_path', type=str, required=True, help='Path to the MLOgraphy predictions directory.')
-    
+    parser.add_argument('--gt_path', type=str, required=True, help='Path to the ground truth images directory.')
+    parser.add_argument('--gt_output_path', type=str, required=True, help='Path to the ground truth output directory.')
+
     args = parser.parse_args()
 
-    mlography++_path = args.mlography++_path
+    mlography_plus_path = args.mlography_plus_path
     zones_path = args.zones_path
     output_path = args.output_path
     mlography_path = args.mlography_path
+    gt_path = args.gt_path
+    gt_output_path = args.gt_output_path
 
-    mlography++_128_out = os.path.join(output_path, 'mlography++_predictions_squares_128')
-    cmlography++_256_path = os.path.join(output_path, 'mlography++_unified_crops_256')
-    process_images(mlography++_path, zones_path, mlography++_128_out)
-    create_directory(mlography++_256_path)
-    unify_crops(output_path, mlography++_256_path)
+    mlography_plus_128_out = os.path.join(output_path, 'mlography_plus_predictions_squares_128')
+    mlography_plus_256_path = os.path.join(output_path, 'mlography_plus_unified_crops_256')
+    process_images(mlography_plus_path, zones_path, mlography_plus_128_out)
+    create_directory(mlography_plus_256_path)
+    unify_crops(mlography_plus_128_out, mlography_plus_256_path)
 
-    
     mlography_128_out = os.path.join(output_path, 'mlography_predictions_squares_128')
     mlography_256_path = os.path.join(output_path, "mlography_predictions_unified_crops_256")
     process_images(mlography_path, zones_path, mlography_128_out)
     create_directory(mlography_256_path)
-    unify_crops(output_path_mlography, mlography_256_path)
+    unify_crops(mlography_128_out, mlography_256_path)
 
-    gt_path = os.path.join(output_path, "GT_256_crops")
-    create_directory(gt_path)
-    unify_crops(gt_path, gt_path)
+    # Handling ground truth path and crops
+    create_directory(gt_output_path)
+    unify_crops(gt_path, gt_output_path)
+
     # Apply Guo-Hall thinning to the masks 
-    process_img(mlography++_256_path, mlography++_256_path)
-    process_img(gt_path, gt_path)
+    process_img(mlography_plus_256_path, mlography_plus_256_path)
+    process_img(gt_output_path, gt_output_path)
 
 if __name__ == "__main__":
     main()
